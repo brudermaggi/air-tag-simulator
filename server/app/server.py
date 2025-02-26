3b00cc9116a317fdea8a999a9fced9a6df74f13c
BruderMaggi <jonathan.frank03@gmail.com>
2025-02-26 17:27:32 +0100

    edit

--- server/app/server.py
+++ server/app/server.py
@@ -2,9 +2,7 @@ from fastapi import FastAPI, HTTPException, Body
 import requests
 import mysql.connector
 from pydantic import BaseModel
-from typing import Dict,Any
 from fastapi.middleware.cors import CORSMiddleware
-import time
 import json
 
 
